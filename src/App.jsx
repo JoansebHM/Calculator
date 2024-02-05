bdcbbf087cb1dc81ed1b2edba5f631d01ddd769c
JoansebHM <joan.herrera481@pascualbravo.edu.co>
2024-02-05 14:19:39 -0500

    Fix responsive issue

--- src/App.jsx
+++ src/App.jsx
@@ -60,25 +60,35 @@ const App = () => {
         <input type="text" value={state} readOnly />
       </form>
       <div className='calculator-buttons'>
+      <div className='button-section'>
         <button onClick={() => handleButtonClick('AC')}>AC</button>
         <button onClick={() => handleButtonClick('DE')}>DE</button>
         <button onClick={() => handleButtonClick('.')}>.</button>
         <button onClick={() => handleButtonClick('/')}>÷</button>
+      </div>
+      <div className='button-section'>
         <button onClick={() => handleButtonClick('7')}>7</button>
         <button onClick={() => handleButtonClick('8')}>8</button>
         <button onClick={() => handleButtonClick('9')}>9</button>
         <button onClick={() => handleButtonClick('*')}>x</button>
+      </div>
+      <div className='button-section'>
         <button onClick={() => handleButtonClick('4')}>4</button>
         <button onClick={() => handleButtonClick('5')}>5</button>
         <button onClick={() => handleButtonClick('6')}>6</button>
         <button onClick={() => handleButtonClick('+')}>+</button>
+      </div>
+      <div className='button-section'>
         <button onClick={() => handleButtonClick('1')}>1</button>
         <button onClick={() => handleButtonClick('2')}>2</button>
         <button onClick={() => handleButtonClick('3')}>3</button>
         <button onClick={() => handleButtonClick('-')}>-</button>
+      </div>
+      <div className='button-section'>
         <button onClick={() => handleButtonClick('00')}>00</button>
         <button onClick={() => handleButtonClick('0')}>0</button>
         <button onClick={() => handleButtonClick('=')}>=</button>
+      </div>
       </div>
     </div>
   );
